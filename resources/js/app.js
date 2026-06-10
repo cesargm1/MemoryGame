171da0b505e559ca3b7c7f577226c6877ebef2ef
cesargm1 <cg.gtav@gmail.com>
2026-06-10 14:52:50 +0200

    memoryGameCounter

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,36 @@
-//
+
+const reset = document.querySelector('.reset')
+const start = document.querySelector('.start');
+const  timerValue = document.querySelector('.timer-value');
+
+
+ let  counter = 0;
+ let interval = null
+
+const initGame = () => {
+
+
+  interval = setInterval(() => {
+        
+
+         if(counter < 100) {
+            counter = counter +1
+           timerValue.innerHTML = counter
+
+         } else {
+         clearInterval(interval)
+         counter = 0
+         timerValue.innerHTML = counter;
+         }
+    }, 1000);
+}
+
+const resetGame = () => {
+    clearInterval(interval)
+    interval = null
+     counter = 0
+    timerValue.innerHTML = counter;
+}
+
+start.addEventListener('click', initGame)
+reset.addEventListener('click', resetGame)
---
+++ resources/views/game/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juego de memoria</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <main>
+        <h1>Juego de memoria</h1>
+            <section class="actions">
+            <button class="start">Empezar juego</button>
+            <button class="reset">resetear</button>
+        </section>
+
+        <div class="timer-section">
+            <span class="timer">Tiempo:</span>
+            <span class="timer-value">0</span>
+        </div>
+
+        <section class="board"></section>
+    </main>
+</body>
+</html>
