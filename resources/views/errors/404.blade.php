6eb93be3b9a30fc8dd9d0d13b52470c2da3d6701
cesar_css_love <cg.gtav@gmail.com>
2026-07-20 18:36:18 +0200

    page 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404</title>
+        @vite(['resources/css/error/404.css'] )
+
+</head>
+<body>
+
+<div class="container">
+    <h1>404</h1>
+    <h2>¡Ups! Página no encontrada</h2>
+    <p>
+        La página que buscas no existe, fue movida o eliminada.
+    </p>
+
+    <a href="/" class="btn">Volver al inicio</a>
+</div>
+
+</body>
+</html>
